5b4a20ca9b375296b99f95f5b554119bd322c9c9
Cleverson Rocha <cleverson.rocha@minu.co>
2022-02-22 19:42:28 -0300

    Iniciando repo

---
+++ s01_introducao/app.js
@@ -0,0 +1,10 @@
+let mostrarSite = true;
+const site = 'www.giadoprogramador.com'
+
+console.log('Olá Mundo!');
+console.log('Meu nome é Cleverson');
+console.log('E estou aprendendo Node.js com o Guia do programador');
+
+if (mostrarSite) {
+  console.log(site);
+};
