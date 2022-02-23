6fbead983a28d9a3d976629efcb99413db0d4855
Cleverson Rocha <cleverson.rocha@minu.co>
2022-02-23 10:16:45 -0300

    Parametro opcional

--- s02_http/express/index.js
+++ s02_http/express/index.js
@@ -2,21 +2,34 @@ const express = require('express'); // Importando o express
 const app = express(); // Iniciando o express
 
 app.get('/', function (req, res) {
-  res.send('Bem vindo ao guia do programador')
+  res.send('Bem vindo ao guia do programador');
   // res.send('Outra resposta') // A resposta só pode ser enviada 1x após a chamada
 });
 
 // Rotas
-app.get('/blog', function (req, res) {
-  res.send('Bem vindo ao meu blog!')
+app.get('/blog/:artigo?', function (req, res) { //Parâmetro opcional => /:<param>
+  let artigo = req.params.artigo;
+  if (artigo) {
+res.send(`<h2>Artigo: ${artigo}</h2>`)
+  } else {
+    res.send('Bem vindo ao meu blog!');
+  }
 })
 
 app.get('/canal/youtube', function (req, res) {
-  res.send('Bem vindo ao meu canal!')
+  res.send('Bem vindo ao meu canal!');
 })
 
-app.get('/ola', function (req, res) {
-  res.send('<h1>Oi</h1>')
+app.get('/ola/:nome/:empresa', function (req, res) {
+  /*
+   Para criar um parâmetro => :<param> e ao utilizar um parametro, é obrigatório digitar alguma
+   informação após abarra
+   req => Dados enviados pelo usuário
+   res => Resposta que vai ser enviada para o usuário
+  */
+  let nome = req.params.nome;
+  let empresa = req.params.empresa;
+  res.send(`<h1>Oi ${nome} do ${empresa}</h1>`);
 })
 
 //Criando servidor
