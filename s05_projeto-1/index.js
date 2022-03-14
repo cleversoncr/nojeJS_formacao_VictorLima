5b2c136e445cf10145525227b4132cd0f09c949e
Cleverson Rocha <cleverson.rocha@minu.co>
2022-03-14 10:11:33 -0300

    sql server

--- s05_projeto-1/index.js
+++ s05_projeto-1/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+//Estou dizendo para o Express usar o EJS como View engine
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 
+//Body parser - captura de dados do formulário
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//Rotas
 app.get('/', (req, res) => {
 
   res.render('index');
@@ -13,10 +20,13 @@ app.get('/perguntar', (req, res) => {
   res.render('perguntar');
 });
 
-app.post('/salvarPergunta',(req, res)=>{
-  res.send('Formulário recebido!');
+app.post('/salvarPergunta', (req, res) => {
+  let titulo = req.body.titulo;
+  let descricao = req.body.descricao;
+  res.send(`Formulário recebido! Titulo: ${titulo} Descrição: ${descricao}`);
 });
 
+//Configuração a porta localhost
 app.listen(8080, () => {
   console.log('App rodando!');
 });
