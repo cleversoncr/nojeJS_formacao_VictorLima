756a06b9a6adcf651ac017b77cf5114a873bc861
Cleverson Rocha <cleverson.rocha@minu.co>
2022-03-23 09:57:21 -0300

    Primeiro projeto finalizado

--- s05_projeto-1/index.js
+++ s05_projeto-1/index.js
@@ -3,6 +3,7 @@ const app = express();
 const bodyParser = require('body-parser');
 const connection = require('./database/database');
 const Pergunta = require('./database/Pergunta');
+const Resposta = require('./database/Resposta');
 
 //Database
 connection
@@ -51,15 +52,33 @@ app.post('/salvarPergunta', (req, res) => {
 app.get('/pergunta/:id', (req, res) => {
   let id = req.params.id;
   Pergunta.findOne({
-    where: { id: id }
+    where: { id: id },
   }).then(pergunta => {
     if (pergunta != undefined) {
-      res.render('pergunta', {
-        pergunta: pergunta
+
+      Resposta.findAll({
+        where: { perguntaId: pergunta.id },
+        order: [['id', 'DESC']]
+      }).then(respostas => {
+        res.render('pergunta', {
+          pergunta: pergunta,
+          respostas: respostas
+        });
       });
     } else {
       res.redirect('/');
     }
+  });
+});
+
+app.post('/responder', (req, res) => {
+  let corpo = req.body.corpo;
+  let perguntaId = req.body.pergunta;
+  Resposta.create({
+    corpo: corpo,
+    perguntaId: perguntaId
+  }).then(() => {
+    res.redirect('/pergunta/' + perguntaId)
   });
 });
 
